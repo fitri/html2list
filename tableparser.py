3a75bde33b11234caa6310041de156c31f714ef4
Fitri <fitrialrahim@gmail.com>
2022-12-18 00:59:48 +0800

    manage to loop find the row list and the data list from the table

--- tableparser.py
+++ tableparser.py
@@ -12,10 +12,24 @@ def gettable(tablepath):
 	with open(tablepath) as table:
 		return table.read()
 
+#call function with passed arg
 htmltable = gettable(args.htmltable)
+#remove tab, space and newline then reassigned
+htmltable = re.sub(r'\s+', '', htmltable)
 
-tablelist = re.findall(r'\<table.*?/table\>*?',htmltable)
+tablelist = re.findall(r'\<table.*?/table\>',htmltable)
 
-print(tablelist)
-print(f"Total available table {len(tablelist)}")
+print(f"Found {len(tablelist)} table in the html")
 
+#loop for table to get row
+for table in tablelist:
+	tablerow = []
+	rowlist = re.findall(r'\<tr.*?/tr\>', table)
+	tablerow.append(rowlist)
+	
+	#loop row to get data
+	for tabledata in rowlist:
+		datalist = re.findall(r'\<th.*?/th\>|\<td.*?/td\>', tabledata)
+	
+	print(f"This table contain {len(tablerow[0])} row")
+	print(f"Each row contain {len(datalist)} data")
